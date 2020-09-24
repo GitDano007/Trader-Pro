9a90cfc5a8c85ee86615cae8b5fdc362b9706b2d
Anh Nguyen <anhnguyendon92@gmail.com>
2020-09-23 19:49:32 -0700

    Routes

--- server.js
+++ server.js
@@ -1,12 +1,45 @@
 // Importing libraries
 const express = require('express');
 const app = express();
-const expbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const path = require('path');
+const request = require('request');
 
-// Importing files
-const routes = require('./routes/handlers');
+const PORT = process.env.PORT || 5000;
 
 
-// Sending static files with Express 
-app.use(express.static('public'));
+// API key pk_f37fe7e4cbe74f4a85a02621ccabbc00
+// create call_api function
+function call_api(){
+
+
+request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_f37fe7e4cbe74f4a85a02621ccabbc00', { json: true }, (err, res, body) => {
+    if (err) {return console.log(err);}
+    console.log(body);
+    if (res.statusCode === 200){
+       // console(body);
+       return body
+    }
+}); 
+}
+// Set Handlebars Middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Set handlebar routes
+app.get('/', function (req, res) {
+    const api = call_api();
+    res.render('home');
+});
+
+// create market page route
+app.get('/market.html', function (req, res){
+    res.render('market');
+})
+
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(PORT, () => console.log('Server Listening on port ' + PORT));
