33ec05dce1b0689be591a4e7242f8ee5678dcb50
Moses Kamara <kamara.moses@yahoo.com>
2020-09-25 12:23:52 -0700

    update the seeds.sql file

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,12 +1,12 @@
-var unirest = require("unirest");
-var req = unirest("GET", "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-summary");
+var unirest = require('unirest');
+var req = unirest('GET', 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-summary');
 req.query({
-	"region": "US"
+	'region': 'US'
 });
 req.headers({
-	"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
-	"x-rapidapi-key": "c19e0e157amsh230c2f6a8d7bf80p132affjsn40ac5c914de6",
-	"useQueryString": true
+	'x-rapidapi-host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
+	'x-rapidapi-key': 'c19e0e157amsh230c2f6a8d7bf80p132affjsn40ac5c914de6',
+	'useQueryString': true
 });
 req.end(function (res) {
 	if (res.error) throw new Error(res.error);
