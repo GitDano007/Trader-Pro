63c3f0a74a7361a2bc18ef5f924405e5629f0f18
Donald Chromy <dgchromy@gmail.comglobal user.emailgit config user.emailgit config --global user.email dgchromy@gmail.com>
2020-10-03 16:23:07 -0700

    serve.js

--- server.js
+++ server.js
@@ -68,5 +68,38 @@ app.use(routes);
 	});
 
 
+	var req = unirest("GET", "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-trending-tickers");
+    req.query({
+    "region": "US"
+});
+    req.headers({
+    "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+    "x-rapidapi-key": "0e63cf5abcmsh4d3233bfb976d24p1d7311jsne67624f44596",
+    "useQueryString": true
+});
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+	
+    // console.log(res.body.finance.result.quotes);
+	
+	req.end(function (res) {
+		if (res.error) throw new Error(res.error);
+	
+		for (let i = 0; i < res.body.finance.result[0].quotes.length; i++) {
+			const finance = {
+				symbol: res.body.finance.result[0].quotes[i].symbol,
+				longName: res.body.finance.result[0].quotes[i].longName,
+				regularMarketPrice: res.body.finance.result[0].quotes[i].regularMarketPrice,
+				regularMarketChangePercent: res.body.finance.result[0].quotes[i].regularMarketChangePercent,
+					   
+			} 
+			console.log(finance);
+		}
+
+	})
+	
+
+
+	
     app.listen(PORT, () => console.log('Server listening on: http://localhost:', + PORT))
 
