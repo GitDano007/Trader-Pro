f648a78fe0fec3c949d0a0c8b5e08bb3108cb5e4
Dano Seese <dpseese@yahoo.com>
2020-10-05 11:35:09 -0700

    updating for Heroku Deploy

--- server.js
+++ server.js
@@ -12,6 +12,25 @@ const db = require("./models");
 
 const PORT = process.env.PORT || 6969;
 
+const mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        password: 'password',
+        database: 'trading_accountDB'
+    });
+};
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+});
+
 
 app.use(express.static("public"));
 
