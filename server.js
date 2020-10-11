c6f81aebe1da24b0ab510f04732fc5145a8e86ba
Jonathan Bower <bowerpower85@gmail.com>
2020-10-10 22:03:28 -0700

    Working on code to append stock data to cards.

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const connection = mysql.createConnection({
 	host: "localhost",
 	port: 3306,
 	user: "root",
-	password: "password",
+	password: "HappyLittleTrees",
 	database: "trading_accountDB"
   });
   
@@ -76,10 +76,32 @@ app.get('/watchlist', (req, res) => {
 				name: data[i].dataValues.short_name
 			});
 		}
-
 		res.render('watchlist', { short_names: names });
 	})
 	
+});
+
+app.get('/watchlist', (req, res) => {
+   db.All_stock.findAll({}).then((data) => {
+	   const stocks = [];
+
+	   for(let i = 0; i <data.length; i++) {
+
+		   stocks.push({
+			   id: data[i].dataValues.id,
+			   name: data[i].dataValues.short_name,
+			   symbol: data[i].dataValues.stock_symbol,
+			   currentPrice: data[i].dataValues.stock_current_price,
+			   dailyHigh: data[i].dataValues.stock_daily_high,
+			   dailyLow: data[i].dataValues.stock_daily_low,
+			   yearHigh: data[i].dataValues.stock_year_high,
+			   yearLow: data[i].dataValues.stock_year_low
+
+		   });
+		   
+	   }
+	   res.render('watchlist', { short_names: stocks, stock_symbols: stocks, stock_current_prices: stocks, stock_daily_highs: stocks, stock_daily_lows: stocks, stock_year_highs: stocks, stock_year_lows: stocks  })
+   })  
 });
 
 //routes
